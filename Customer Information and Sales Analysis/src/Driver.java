d57bc4909a0f0162424fd00d81d3b9029212db79
323863381 <323863381@C-2V5ZJM1.tdsb.on.ca>
2018-05-15 14:36:48 -0400

    Initial

---
+++ Customer Information and Sales Analysis/src/Driver.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Welcome to the customer information and sales analysis system.");
+		System.out.println("Please enter the number corresponding to the option you wish to choose.");
+		System.out.println("1. Enter new customer information.");
+		
+		int option = input.nextInt();
+		if (option == 1){
+			System.out.println("How many customers would you like the enter into the system? Please respond with an integer.");
+		}
+	}
+
+}
